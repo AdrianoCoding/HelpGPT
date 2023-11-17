d535ffc0f43eab276930fc68c587f39422cc2182
Adriano J <adrianojbarros5@gmail.com>
2023-11-17 10:19:23 -0300

    commit-2

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 import requests  
 
 url = "https://freegptapi.hop.sh/neural/api" 
-params = {"query": input('your question: ') }
-print('\n\nThinking...\n\n')
-response = requests.get(url, params=params) 
-data = response.json()  
-
-print(data)
+while True:
+        user_input = input('Sua pergunta: ')
+        params = {"query": user_input}
+        if user_input.lower() == 'exit':
+            break
+        
+        print('\n\n\nThinking\n\n\n')
+        response = requests.get(url, params=params) 
+        data = response.json()  
+        print(data)
+        
+        print('\n\n','for exit press: ctrl + z')
