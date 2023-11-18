be5f7d3020fea91b3deb9dd8bfc75287195d2ed4
Adriano J <adrianojbarros5@gmail.com>
2023-11-18 13:04:27 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import requests
 import time
 from colorama import Fore
+import webbrowser
+import os
+
 
 url = "https://freegptapi.hop.sh/neural/api"
 ascii_art = '''
@@ -12,21 +15,52 @@ ascii_art = '''
 //    / / ((____   // //           ((____/ / //          / /    
 '''
 
-print(Fore.BLUE + ascii_art)
+print(Fore.WHITE + ascii_art)
 
-print(Fore.BLUE + 'Welcome to Help GPT, the script that assists you with AI in resolving queries regarding EF Core and Z.EntityFramework.Extensions!')
+print(Fore.WHITE + 'Welcome to Help GPT, the script that assists you with AI in resolving queries regarding EF Core and Z.EntityFramework.Extensions!')
 time.sleep(4)
-print('\n\n')
+print(Fore.WHITE + 'select an option')
+print(Fore.WHITE + '\n [1] Assistent with IA \n [2] Config \n [3] Github (credits) \n [4] Exit')
+num = int(input("Typing a option: "))
 
-while True:
-    user_input = input(Fore.RED + 'Your question: ')
-    params = {"query": user_input}
-    if user_input.lower() == 'exit':
-        break
+if num == '1':
+    while True:
+        user_input = input(Fore.RED + 'Your question: ')
+        params = {"query": user_input}
+        if user_input.lower() == 'exit':
+            break
 
-    print('\n\n\nThinking...\n')
-    print('For exit press: ctrl + z')
-    response = requests.get(url, params=params)
-    data = response.json()
-    print(Fore.GREEN + str(data))  # converted data to string for printing
+        print('\n\n\nThinking...')
+        print('For exit press: ctrl + z')
+        response = requests.get(url, params=params)
+        data = response.json()
+        print(Fore.GREEN + str(data))  # converted data to string for printing
 
+if num == '2':
+    print('Sorry, this option has not yet been developed')
+
+if num == 3:
+    print('visit the github ')
+    print(url)
+    url = 'https://github.com/NeuronNix/HelpGPT'
+    time.sleep(2)
+    webbrowser.open(url)
+
+if num == 4:
+    print('Bye - Bye')
+    ascii_art = '''
+⠀⠀⠀⢀⡴⠟⠛⢷⡄⠀⣠⠞⠋⠉⠳⡄⠀⠀⠀⠀
+⠀⠀⠀⣸⠁⠀⠀⠈⣧⢰⠇⠀⠀⠀⢠⡇⠀⠀⠀⠀
+⠀⠀⠀⠸⣆⠀⠀⠀⠘⣿⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠹⣦⠀⠀⠀⠘⡄⠀⠀⠀⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⡴⠚⠙⠳⣀⡴⠂⠁⠒⢄⠀⢿⡀⠀⠀⠀⠀
+⠀⠀⢸⡇⠀⢀⠔⠉⠀⠀⠀⡀⠀⠂⠘⣇⠀⠀⠀⠀
+⠀⠀⠀⢳⡀⠘⢄⣀⣀⠠⠶⠄⠀⠀⠀⡿⠀⠀⠀⠀
+⠀⠀⠀⠀⠻⣕⠂⠁⠀⠀⠀⠀⠀⠀⣰⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠹⣅⠒⠀⠒⠂⠐⠒⢉⡼⢁⣤⠀⠀⠀
+⠀⢀⣼⣻⣆⣤⢈⣙⣒⠶⠶⣶⣞⢿⣸⡟⠳⣾⣂⣤
+⠸⢿⣽⠏⢠⡿⠋⣿⣭⢁⣈⣿⣽⣆⠙⢷⣄⠙⠋⠁
+⠀⠀⠀⠀⠛⠃⠰⠿⣤⠄⠀⠸⠷⠟⠀⠀⠁⠀⠀⠀
+'''
+print(ascii_art)
+time.sleep(2)
